069f6f6c094e12e6b6cb1a20d0f59269d31c0ce2
milthuz <david.lafleur@clusterpos.com>
2025-10-28 14:26:05 -0400

    Fix React rendering

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import CommissionTracker from './commission-tracker-cluster-branded';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
