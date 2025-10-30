d70e1bddf41c3b2c7644a1048d250a62745b30f7
milthuz <david.lafleur@clusterpos.com>
2025-10-30 16:27:50 -0400

    Fix import path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import CommissionTracker from './pages/CommissionTrackerV2';
+import CommissionTracker from './pages/commission-tracker-cluster-branded';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
