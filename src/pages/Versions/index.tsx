55f15bfa9559215bacd588c0af07ff01467e3405
milthuz <david@clustersystems.com>
2025-11-03 19:23:39 -0500

    fix: Restore gear icon and proper colors in help dropdown

--- src/pages/Versions/index.tsx
+++ src/pages/Versions/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import Breadcrumb from '../../components/Breadcrumbs/Breadcrumb';
 
 const Versions: React.FC = () => {
   return (
-    <>
-      <Breadcrumb pageName="Version History" />
+    <div className="mx-auto max-w-270">
+      <div className="mb-6">
+        <h2 className="text-title-md2 font-semibold text-black dark:text-white">
+          Version History
+        </h2>
+      </div>
 
       <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
         <div className="border-b border-stroke py-4 px-6.5 dark:border-strokedark">
@@ -132,7 +135,7 @@ const Versions: React.FC = () => {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
