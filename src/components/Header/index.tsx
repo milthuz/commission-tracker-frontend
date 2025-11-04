0654637a65069bc92a04039f3d2f74d22c4f2d4e
milthuz <david@clustersystems.com>
2025-11-03 20:27:31 -0500

    refactor: Remove dark mode toggle and help icon from header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,75 +1,74 @@
-import { useState } from 'react';
-import DarkModeSwitcher from './DarkModeSwitcher';
+import { Link } from 'react-router-dom';
+import DropdownNotification from './DropdownNotification';
 import DropdownUser from './DropdownUser';
-import DropdownHelp from './DropdownHelp';
+import LogoIcon from '../../images/logo/logo-icon.svg';
 
-interface HeaderProps {
+const Header = (props: {
   sidebarOpen: string | boolean | undefined;
-  setSidebarOpen: (arg: boolean) => void;
-}
-
-const Header = ({ sidebarOpen, setSidebarOpen }: HeaderProps) => {
+  setSidebarOpen: (arg0: boolean) => void;
+}) => {
   return (
-    <header className="sticky top-0 z-999 flex bg-white drop-shadow-1 dark:drop-shadow-none dark:bg-boxdark">
-      <div className="flex flex-grow items-center justify-between px-4 py-4 md:px-6 2xl:px-11">
-        <div className="flex items-center gap-2 sm:gap-4 lg:-ml-64">
+    <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
+      <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11">
+        <div className="flex items-center gap-2 sm:gap-4 lg:hidden">
           {/* <!-- Hamburger Toggle BTN --> */}
           <button
             aria-controls="sidebar"
             onClick={(e) => {
               e.stopPropagation();
-              setSidebarOpen(!sidebarOpen);
+              props.setSidebarOpen(!props.sidebarOpen);
             }}
-            className="z-99999 block rounded-sm border border-stroke bg-white p-1.5 shadow-sm dark:border-strokedark dark:bg-boxdark sm:hidden"
+            className="z-99999 block rounded-sm border border-stroke bg-white p-1.5 shadow-sm dark:border-strokedark dark:bg-boxdark lg:hidden"
           >
             <span className="relative block h-5.5 w-5.5 cursor-pointer">
               <span className="du-block absolute right-0 h-full w-full">
                 <span
-                  className={`relative top-0 left-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-[0] duration-200 ease-in-out dark:bg-white ${
-                    !sidebarOpen && '!w-full delay-300'
+                  className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-[0] duration-200 ease-in-out dark:bg-white ${
+                    !props.sidebarOpen && '!w-full delay-300'
                   }`}
                 ></span>
                 <span
-                  className={`relative top-0 left-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-150 duration-200 ease-in-out dark:bg-white ${
-                    !sidebarOpen && 'delay-400 !w-full'
+                  className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-150 duration-200 ease-in-out dark:bg-white ${
+                    !props.sidebarOpen && 'delay-400 !w-full'
                   }`}
                 ></span>
                 <span
-                  className={`relative top-0 left-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-200 duration-200 ease-in-out dark:bg-white ${
-                    !sidebarOpen && '!w-full delay-500'
+                  className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-200 duration-200 ease-in-out dark:bg-white ${
+                    !props.sidebarOpen && '!w-full delay-500'
                   }`}
                 ></span>
               </span>
               <span className="absolute right-0 h-full w-full rotate-45">
                 <span
-                  className={`du-block absolute right-0 top-0 my-1 block h-0.5 w-full rounded-sm bg-black delay-300 duration-200 ease-in-out dark:bg-white ${
-                    !sidebarOpen && '!h-0 delay-[0]'
+                  className={`absolute left-2.5 top-0 block h-full w-0.5 rounded-sm bg-black delay-300 duration-200 ease-in-out dark:bg-white ${
+                    !props.sidebarOpen && '!h-0 !delay-[0]'
                   }`}
                 ></span>
                 <span
-                  className={`du-block absolute right-0 top-0 my-1 block h-0.5 w-full rounded-sm bg-black delay-400 duration-200 ease-in-out dark:bg-white ${
-                    !sidebarOpen && '!h-0 delay-200'
-                  }`}
-                ></span>
-                <span
-                  className={`du-block absolute right-0 top-0 my-1 block h-0.5 w-full rounded-sm bg-black delay-500 duration-200 ease-in-out dark:bg-white ${
-                    !sidebarOpen && '!h-0 delay-300'
+                  className={`delay-400 absolute left-0 top-2.5 block h-0.5 w-full rounded-sm bg-black duration-200 ease-in-out dark:bg-white ${
+                    !props.sidebarOpen && '!h-0 !delay-200'
                   }`}
                 ></span>
               </span>
             </span>
           </button>
           {/* <!-- Hamburger Toggle BTN --> */}
+
+          <Link className="block flex-shrink-0 lg:hidden" to="/">
+            <img src={LogoIcon} alt="Logo" />
+          </Link>
         </div>
 
-        <div className="flex items-center justify-end gap-3 2xl:gap-7">
-          {/* <!-- Dark Mode Toggler --> */}
-          <DarkModeSwitcher />
-          {/* <!-- Dark Mode Toggler --> */}
+        <div className="hidden sm:block">
+          {/* Empty div for spacing - you can add search or other elements here */}
+        </div>
 
-          {/* <!-- Help & Settings Dropdown --> */}
-          <DropdownHelp sidebarOpen={sidebarOpen} />
-          {/* <!-- Help & Settings Dropdown --> */}
+        <div className="flex items-center gap-3 2xsm:gap-7">
+          <ul className="flex items-center gap-2 2xsm:gap-4">
+            {/* <!-- Notification Menu Area --> */}
+            <DropdownNotification />
+            {/* <!-- Notification Menu Area --> */}
+          </ul>
 
           {/* <!-- User Area --> */}
           <DropdownUser />
